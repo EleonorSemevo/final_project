75711a42885c858cb5914f46c5ab840dced386ff
EleonorSemevo <eleonorsemevo@gmail.com>
2021-10-21 12:40:18 +0100

    la fonction de recherche est terminée au niveau de pharmacie

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,9 +1,87 @@
 class PharmaciesController < InheritedResources::Base
 
-  private
+def index
+  @pharmacies = Pharmacy.all
+  if params[:pharmacy].present?
+    name = params[:pharmacy][:name]
+    area_id = params[:pharmacy][:area_id]
+    town_id = params[:pharmacy][:town_id]
+    if town_id !=""
+      town = Area.find(town_id.to_i).town
+    end
 
+    if name !='' && area_id !='' && town_id != ''
+      search_for_name_town_area_(name,area_id, town)
+    elsif  name !='' && area_id !='' && town_id == ''
+      search_for_name_area(name,area_id)
+    elsif  name !='' && area_id =='' && town_id != ''
+      search_name_town(name, town)
+    elsif  name =='' && area_id !='' && town_id != ''
+      search_area_town(area_id,town)
+    elsif name !='' && area_id =='' && town_id == ''
+      @pharmacies = Pharmacy.search_name(name)
+     elsif name =='' && area_id !='' && town_id == ''
+       @pharmacies = Pharmacy.search_area(area_id)
+    elsif name =='' && area_id =='' && town_id != ''
+      @pharmacies = search_for_town(town)
+   end
+ end
+end
+
+  private
+    def search_for_name_town_area_(name,area_id, town)
+      result = Pharmacy.search_name(name).search_area(area_id)
+      interm = []
+      # rechercher selon town
+      result.each do |pharmacy|
+         if pharmacy.area.town == town
+           interm.push(pharmacy)
+         end
+      end
+      @pharmacies = interm
+    end
+
+    def search_for_name_area(name,area_id)
+      @pharmacies = Pharmacy.search_name(name).search_area(area_id)
+    end
+
+    def search_name_town(name, town)
+      result = Pharmacy.search_name(name)
+      interm = []
+      # rechercher selon town
+      result.each do |pharmacy|
+         if pharmacy.area.town == town
+           interm.push(pharmacy)
+         end
+      end
+      @pharmacies = interm
+    end
+
+    def search_area_town(area_id,town)
+      result = Pharmacy.search_area(area_id)
+      interm = []
+      # rechercher selon town
+      result.each do |pharmacy|
+         if pharmacy.area.town == town
+           interm.push(pharmacy)
+         end
+      end
+      @pharmacies = interm
+    end
+
+    def search_for_town(town)
+      result = Pharmacy.all
+      interm = []
+      # rechercher selon town
+      result.each do |pharmacy|
+         if pharmacy.area.town == town
+           interm.push(pharmacy)
+         end
+      end
+      @pharmacies = interm
+    end
     def pharmacy_params
-      params.require(:pharmacy).permit(:name, :area_id, :all_nigth, :googlemap_link, :number1, :number2)
+      params.require(:pharmacy).permit(:name, :area_id, :all_nigth, :googlemap_link, :number1, :number2, :town_id)
     end
 
 end
--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -1,3 +1,6 @@
 class Pharmacy < ApplicationRecord
   belongs_to :area
+
+  scope :search_name, ->(name){where('name like ?', name)}
+  scope :search_area, ->(area_id){where('area_id = ?', area_id)}
 end
