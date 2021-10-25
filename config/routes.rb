c95844887241d8189d77b783e4628df004ce3d3e
EleonorSemevo <eleonorsemevo@gmail.com>
2021-10-25 14:33:03 +0100

    la connection faite sans restriction

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :sessions, only: [:new, :create, :show, :destroy]
   resources :users
   resources :blood_bank_insurances
   resources :hospital_insurances
